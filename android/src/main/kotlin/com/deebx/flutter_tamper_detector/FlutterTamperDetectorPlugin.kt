6e62dea580ebc6cbae88057897db94fe4aa2fafb
Kauê Murakami <kauetmurakami@gmail.com>
2025-03-18 17:20:42 -0300

    Added new parameter for isHooked and isRooted : uninstallIfTrue This parameter use the self rooted device to uninstall app with admin permissions Added CHANGELOG and change README

--- android/src/main/kotlin/com/deebx/flutter_tamper_detector/FlutterTamperDetectorPlugin.kt
+++ android/src/main/kotlin/com/deebx/flutter_tamper_detector/FlutterTamperDetectorPlugin.kt
@@ -2,6 +2,10 @@ package com.deebx.flutter_tamper_detector
 
 import android.content.Context
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -25,38 +29,78 @@ class FlutterTamperDetectorPlugin: FlutterPlugin, MethodCallHandler {
   }
 
     override fun onMethodCall(call: MethodCall, result: Result) {
+        val exitProcessIfTrue = call.argument<Boolean>("exitProcessIfTrue") ?: false
+        val uninstallIfTrue = call.argument<Boolean>("uninstallIfTrue") ?: false
+
         when (call.method) {
             "isRooted" -> {
                 val isRooted = RootChecker.isDeviceRooted()
-                handleExitCondition(call, isRooted)
+                handleExitOrUninstall(isRooted, exitProcessIfTrue, uninstallIfTrue)
                 result.success(isRooted)
             }
             "isHooked" -> {
                 val isHooked = HookDetector.check()
-                handleExitCondition(call, isHooked)
+                handleExitOrUninstall(isHooked, exitProcessIfTrue, uninstallIfTrue)
                 result.success(isHooked)
             }
             "isEmulator" -> {
                 val isEmulator = IsEmulator.check()
-                handleExitCondition(call, isEmulator)
+                handleExitOrUninstall(isEmulator, exitProcessIfTrue, false)
                 result.success(isEmulator)
             }
             "isDebug" -> {
                 val isDebug = IsDebug.check(context)
-                handleExitCondition(call, isDebug)
+                handleExitOrUninstall(isDebug, exitProcessIfTrue, false)
                 result.success(isDebug)
             }
             else -> result.notImplemented()
         }
     }
- private fun handleExitCondition(call: MethodCall, shouldExit: Boolean) {
-    val exitProcessIfTrue = call.argument<Boolean>("exitProcessIfTrue") ?: false
 
-    if (exitProcessIfTrue && shouldExit) {
-        exitProcess(0)
+    private fun handleExitOrUninstall(shouldExit: Boolean, exitProcessIfTrue: Boolean, uninstallIfTrue: Boolean) {
+        if (uninstallIfTrue && shouldExit) {
+            uninstallApp()
+        }
+        if (exitProcessIfTrue && shouldExit) {
+            exitProcess(0)
+        }
     }
-}
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    // private fun handleExitCondition(call: MethodCall, shouldExit: Boolean) {
+    //     val exitProcessIfTrue = call.argument<Boolean>("exitProcessIfTrue") ?: false
+
+    //     if (exitProcessIfTrue && shouldExit) {
+    //         exitProcess(0)
+    //     }
+    // }
+
+    private fun uninstallApp() {
+        try {
+            // Tentativa de desinstalação via root (comando "su")
+            val packageName = context.packageName
+            val process = Runtime.getRuntime().exec("su")
+            val outputStream = process.outputStream
+            outputStream.write("pm uninstall $packageName\n".toByteArray())
+            outputStream.flush()
+            outputStream.close()
+
+            // Verificar o resultado da desinstalação
+            val reader = BufferedReader(InputStreamReader(process.inputStream))
+            val result = reader.readLine()
+            reader.close()
+
+
+            // Se a desinstalação via root falhar, tenta desinstalar normalmente
+            if (result == null || !result.contains("Success")) {
+                val intent = Intent(Intent.ACTION_DELETE)
+                intent.data = Uri.parse("package:$packageName")
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                context.startActivity(intent)
+            }
+        } catch (e: Exception) {
+            e
+        }
+    }
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 }
--- lib/src/is_rooted.dart
+++ lib/src/is_rooted.dart
@@ -14,29 +14,33 @@ import 'package:flutter/services.dart';
 /// ```
 class IsRooted {
   /// The method channel used for communicating with the native platform.
-  static const MethodChannel _channel = MethodChannel(
-    'flutter_tamper_detector',
-  );
+  static const MethodChannel _channel = MethodChannel('flutter_tamper_detector');
 
-  /// Checks whether the device is rooted.
+  /// Checks whether the app is being hooked by malicious tools.
   ///
-  /// Returns `true` if the device is rooted, otherwise `false`.
+  /// Returns `true` if hooking is detected, otherwise `false`.
   ///
   /// If an error occurs while checking, it catches the exception and returns `false`.
   ///
-  /// The [exitProcessIfTrue] parameter, which is `false` by default, determines whether the app should terminate the process
-  /// if the device is rooted. If set to `true`, the app will attempt to terminate the process on the native side, potentially
+  /// The [exitProcessIfTrue] parameter, which defaults to `false`, determines whether the app should terminate the process
+  /// if hooking is detected. If set to `true`, the app will attempt to terminate the process on the native side, potentially
   /// using methods like `exitProcess(0)` depending on the implementation in Kotlin/Java.
   ///
-  /// [exitProcessIfTrue] helps enforce stricter security measures when detecting rooted devices, allowing the app to exit
-  /// automatically in case of tampering.
+  /// The [uninstallIfTrue] parameter, which also defaults to `false`, determines whether the app should attempt to automatically
+  /// uninstall itself if hooking is detected. If set to `true`, the app will try to uninstall via root access. If root
+  /// uninstallation fails, a standard uninstall prompt will be shown to the user.
   ///
-  /// If the [exitProcessIfTrue] flag is set to `true`, it will be passed to the native code to trigger an exit condition.
+  /// Using [uninstallIfTrue] and [exitProcessIfTrue] simultaneously provides a more aggressive approach to mitigate potential
+  /// tampering or reverse-engineering attempts, ensuring stricter security.
+  ///
+  /// If both flags are set to `true`, the app will prioritize attempting uninstallation before terminating the process.
+  ///
   /// If the check fails, the method returns `false`.
-  static Future<bool> check({bool exitProcessIfTrue = false}) async {
+  static Future<bool> check({bool exitProcessIfTrue = false, bool uninstallIfTrue = false}) async {
     try {
       return await _channel.invokeMethod('isRooted', {
             'exitProcessIfTrue': exitProcessIfTrue,
+            'uninstallIfTrue': uninstallIfTrue,
           }) ??
           false;
     } on PlatformException catch (e) {
